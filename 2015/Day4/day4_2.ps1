74b058851191f29202fd2d46588e5a9814aefdb2
Will <36126247+will-86@users.noreply.github.com>
2020-12-13 13:22:07 +0000

    Day4

---
+++ 2015/Day4/day4_2.ps1
@@ -0,0 +1,35 @@
+$base = "yzbqklnj"
+$i = 7000000
+$found = $false
+
+function Get-StringHash {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory, Position=0)]
+		[string]
+		$InputObject
+	)
+
+	process {
+		$strBld = New-Object System.Text.StringBuilder
+		$hasher = [System.Security.Cryptography.HashAlgorithm]::Create("MD5")
+		$hashBytes = $hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($InputObject))
+		foreach ($hb in $hashBytes) {
+			[void]$strBld.Append($hb.ToString("x2"))
+		}
+		Write-Output "$($strBld.ToString())"
+	}
+}
+
+while (-not $found) {
+	if ($i % 100 -eq 0) { Write-Output "Current index: $i"}
+	$inputKey = "$base$i"
+	$hash = Get-StringHash $inputKey
+	if ($hash.Substring(0,6) -eq "000000") {
+		$found = $true
+		Write-Output "Index: $i"
+		Write-Output "Hash: $hash"
+	} else {
+		$i++
+	}
+}
